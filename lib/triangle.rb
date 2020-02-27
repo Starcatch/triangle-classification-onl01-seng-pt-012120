125e2e1d7a7e0cec88ec0ad4f5b4ba32d3aeca4e
Timur Mishiev <timurmishiev@gmail.com>
2020-02-27 02:25:41 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -33,34 +33,3 @@ class TriangleError < StandardError
 end 
 =end 
 
-class TriangleError < StandardError
-end
-
-class Triangle
-
-  attr_accessor :a, :b, :c
-
-  def initialize(a, b, c)
-    @a = a
-    @b = b
-    @c = c
-  end
-
-  def kind
-    sides = [a,b,c]
-    ineq = [a+b > c, a+c > b, b+c > a]
-
-    if sides.min <= 0 || ineq.include?(false)
-      raise TriangleError
-    end
-
-    if a == b && b == c
-      :equilateral
-    elsif a != b && a != c && b != c
-      :scalene
-    elsif (a == b && b != c) || (b == c && c != a) || (a == c && c != b)
-      :isosceles
-    end
-  end
-  
-end
