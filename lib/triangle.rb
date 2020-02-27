08b607a8a8513545403459f6483fe23f87830249
Timur Mishiev <timurmishiev@gmail.com>
2020-02-27 00:42:44 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -19,9 +19,21 @@ class Triangle
     end
   end
   
-  
- 
-  class TriangleError < StandardError
-    # triangle error code
+  def kind
+    if valid?
+      if @triangle_sides.uniq.length == 1
+        return :equilateral
+      elsif @triangle_sides.uniq.length == 2
+        return :isosceles
+      else
+        return :scalene
+      end
+    else
+      raise TriangleError
+    end
   end
+end
+
+class TriangleError < StandardError
+
 end
