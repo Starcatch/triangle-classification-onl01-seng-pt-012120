47b6caeddbaefe4942e428c834a953b4dc656e8b
Timur Mishiev <timurmishiev@gmail.com>
2020-02-27 02:25:16 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,4 +1,4 @@
-class Triangle 
+=begin  class Triangle 
   attr_accessor :s1, :s2, :s3, :kind 
   
   def initialize(s1, s2, s3)
@@ -31,3 +31,36 @@ class TriangleError < StandardError
     puts "not a triangle"
   end 
 end 
+=end 
+
+class TriangleError < StandardError
+end
+
+class Triangle
+
+  attr_accessor :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    sides = [a,b,c]
+    ineq = [a+b > c, a+c > b, b+c > a]
+
+    if sides.min <= 0 || ineq.include?(false)
+      raise TriangleError
+    end
+
+    if a == b && b == c
+      :equilateral
+    elsif a != b && a != c && b != c
+      :scalene
+    elsif (a == b && b != c) || (b == c && c != a) || (a == c && c != b)
+      :isosceles
+    end
+  end
+  
+end
