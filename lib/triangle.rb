8d54709404f07eb0e611470066623db683f61f42
Timur Mishiev <timurmishiev@gmail.com>
2020-02-27 02:18:38 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,2 +1,24 @@
 class Triangle 
-  attr_accessor :s1, :s2, :3, :kind
+  attr_accessor :s1, :s2, :3, :kind 
+  
+  def initialize(s1, s2, s3)
+    @s1 = s1
+    @s2 = s2
+    @s3 = s3
+  end 
+  
+  def kind
+    if (s1 * s2 * s3) == 0 || (s1 + s2) <= s3 || (s2 + s3) <= s1 || (s3 + s1) <= s2
+      begin
+       raise TriangleError
+        puts error.message
+      end 
+      
+    elsif s1 == s2 && s1 == s3
+     self.kind = :equilateral
+     
+    elsif s1 == s2 || s1 == s3 || s2 == s3
+     self.kind = :isosceles
+     
+   else 
+     self.kind = :scal
