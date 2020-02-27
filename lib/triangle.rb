05f4493ec30fa72f4a06435b2e756a076eceed6e
Timur Mishiev <timurmishiev@gmail.com>
2020-02-27 01:27:29 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,7 +9,7 @@ class Triangle
   end 
 
  def kind
-   if (@side1 + @side2) <= @side3 || 
+   if (@side1 + @side2) <= @side3 || (@side2 + @side3) <= @side1
  end 
  
  class TriangleError < StandardError
